018a464f611e79a29f4467866ca296cb08d16866
shahidk.enjay <shahidk.enjay@gmail.com>
2026-07-14 12:06:35 +0530

    fix: Export authClient for SettingsPage

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -2,6 +2,8 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { auth } from "../../../server/src/lib/auth";
 
-export const { signIn, signOut, useSession } = createAuthClient({
+export const authClient = createAuthClient({
   plugins: [inferAdditionalFields<typeof auth>()],
 });
+
+export const { signIn, signOut, useSession } = authClient;
